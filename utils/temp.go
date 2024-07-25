acc9a9dfdac6cf31c85266e4e6a04291c289d016
spacemen0 <frakctures@outlook.com>
2024-07-25 10:32:50 +0200

    add utils to read and clean data

---
+++ utils/temp.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"spacemen0.github.com/helpers"
+	"strconv"
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+type Person struct {
+	ID                string `gorm:"primaryKey;column:nconst"`
+	PrimaryName       string
+	BirthYear         *int
+	DeathYear         *int
+	PrimaryProfession *string  `gorm:"type:text[]"`
+	KnownForTitles    []*Title `gorm:"many2many:filmography;"`
+}
+
+type Title struct {
+	ID             string `gorm:"primaryKey;column:tconst"`
+	TitleType      string
+	PrimaryTitle   string
+	OriginalTitle  string
+	IsAdult        bool
+	StartYear      *int
+	EndYear        *int
+	RuntimeMinutes *int
+	Genres         *string
+	Actors         []*Person `gorm:"many2many:filmography;"`
+}
+
+func main() {
+	helpers.InitDB()
+	db := helpers.GetDB()
+
+	err := readPersons("person.tsv", db)
+	if err != nil {
+		log.Fatalf("failed to read persons: %v", err)
+	}
+}
+
+func readPersons(filename string, db *gorm.DB) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Print(err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // Skip header line
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Split(line, "\t")
+
+		birthYear, deathYear := parseYear(fields[2]), parseYear(fields[3])
+		var profession *string
+		if fields[4] != "\\N" {
+			professionList := strings.Split(fields[4], ",")
+			professionStr := "{" + strings.Join(professionList, ",") + "}"
+			profession = &professionStr
+		}
+
+		person := Person{
+			ID:                fields[0],
+			PrimaryName:       fields[1],
+			BirthYear:         birthYear,
+			DeathYear:         deathYear,
+			PrimaryProfession: profession,
+			KnownForTitles:    []*Title{},
+		}
+
+		if err := db.Create(&person).Error; err != nil {
+			log.Printf("failed to create person: %v", err)
+		}
+		if fields[4] != "\\N" {
+			knownForTitles := strings.Split(fields[5], ",")
+			for _, titleID := range knownForTitles {
+				var title Title
+				if titleID != "\\N" {
+					if err := db.First(&title, "tconst = ?", titleID).Error; err == nil {
+						err := db.Model(&person).Association("KnownForTitles").Append(&title)
+						if err != nil {
+							return err
+						}
+					}
+				}
+
+			}
+		}
+
+	}
+
+	return scanner.Err()
+}
+
+func readTitles(filename string, db *gorm.DB) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // Skip header line
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Split(line, "\t")
+
+		startYear, endYear := parseYear(fields[5]), parseYear(fields[6])
+		runtimeMinutes := parseInt(fields[7])
+		var genres *string
+		if fields[8] != "\\N" {
+			genreList := strings.Split(fields[8], ",")
+			genreStr := "{" + strings.Join(genreList, ",") + "}"
+			genres = &genreStr
+		}
+		isAdult := fields[4] == "1"
+
+		title := Title{
+			ID:             fields[0],
+			TitleType:      fields[1],
+			PrimaryTitle:   fields[2],
+			OriginalTitle:  fields[3],
+			IsAdult:        isAdult,
+			StartYear:      startYear,
+			EndYear:        endYear,
+			RuntimeMinutes: runtimeMinutes,
+			Genres:         genres,
+			Actors:         []*Person{},
+		}
+
+		if err := db.Create(&title).Error; err != nil {
+			log.Printf("failed to create title: %v", err)
+		}
+	}
+
+	return scanner.Err()
+}
+
+func parseYear(yearStr string) *int {
+	if yearStr == "\\N" {
+		return nil
+	}
+	year, err := strconv.Atoi(yearStr)
+	if err != nil {
+		log.Printf("failed to parse year: %v", err)
+		return nil
+	}
+	return &year
+}
+
+func parseInt(intStr string) *int {
+	if intStr == "\\N" {
+		return nil
+	}
+	value, err := strconv.Atoi(intStr)
+	if err != nil {
+		log.Printf("failed to parse int: %v", err)
+		return nil
+	}
+	return &value
+}
+
+func parseString(str string) *string {
+	if str == "\\N" {
+		return nil
+	}
+	return &str
+}
---
+++ utils/tsv_cleaner.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def filter_bad_lines(input_file, output_file):
+    try:
+        # Read the TSV file and ignore bad lines automatically
+        df = pd.read_csv(input_file, sep='\t', dtype=str, on_bad_lines='skip')
+
+
+        # Additional criteria can be added here as needed
+        # Example: Removing rows where a specific column is not a valid integer
+        # filtered_df = filtered_df[filtered_df['some_column'].apply(lambda x: x.isdigit())]
+
+        # Write the filtered data to a new TSV file
+        df.to_csv(output_file, sep='\t', index=False)
+
+        print(f"Filtered file saved to {output_file}")
+
+    except Exception as e:
+        print(f"Error processing the file: {e}")
+
+# Usage example
+input_file = 'title.basics.tsv'
+output_file = 'title.tsv'
+filter_bad_lines(input_file, output_file)
